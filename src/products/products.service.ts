03de27380c5036ca5b2c7a5f080b153d9da25e76
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-31 00:10:37 +0100

    create entity dto and crud

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  private readonly logger = new Logger('ProductsService');
+  constructor(
+    @InjectRepository(Product)
+    private readonly producRepository: Repository<Product>
+  ){}
+
+
+  async create(createProductDto: CreateProductDto) {
+    
+    try {
+
+      //Este codigo se hace para hacer la validacion de que si el slug no es mandado tome lo mismo que viene en el title que es requerido pero como nest y typeorm nos proveen ya una forma mas sencilla se aplica directamente la validacion al final de la entidad como un metodo y un decorador
+      // if (!createProductDto.slug) {
+      //   createProductDto.slug = createProductDto.title
+      //   .toLowerCase()
+      //   .replaceAll(' ', '_')
+      //   .replaceAll("'", '');
+      // }else{
+      //   createProductDto.slug = createProductDto.slug
+      //   .toLowerCase()
+      //   .replaceAll(' ', '_')
+      //   .replaceAll("'", '');
+      // }
+      const product = this.producRepository.create(createProductDto);
+      await this.producRepository.save(product);
+
+      return product
+    } catch (error) {
+      this.handleDBExceptions(error)
+      
+    }
+    
   }
 
+  //Todo: pagination
   findAll() {
-    return `This action returns all products`;
+    
+    return this.producRepository.find({})
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    
+    const product = await this.producRepository.findOneBy({id})
+    if(!product){
+      throw new NotFoundException(`Product with id "${id}" not found`)
   }
+
+  return product
+}
+  
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id)
+    await this.producRepository.remove(product)
+    
+  }
+
+  private handleDBExceptions(error: any){
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.error(error)
+    throw new InternalServerErrorException('Unexpected error, check server logs')
+
   }
 }
